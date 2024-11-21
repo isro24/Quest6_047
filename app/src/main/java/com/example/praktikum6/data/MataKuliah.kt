6fc84d9b7fd0d05c482370d6c490a75f9bdb8f9f
isro24 <alamat_email_anda@example.com>
2024-11-21 20:18:12 +0700

    Menambahkan list objek MataKuliah

--- app/src/main/java/com/example/praktikum6/data/MataKuliah.kt
+++ app/src/main/java/com/example/praktikum6/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.example.praktikum6.data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision",
+    )
 }
