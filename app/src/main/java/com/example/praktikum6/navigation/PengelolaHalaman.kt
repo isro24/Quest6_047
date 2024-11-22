8f4b4968b1897d15ab0d399357d66f5dad034b69
isro24 <alamat_email_anda@example.com>
2024-11-22 09:42:16 +0700

    Menambah fun MahasiswaApp

--- app/src/main/java/com/example/praktikum6/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/praktikum6/navigation/PengelolaHalaman.kt
@@ -1,8 +1,24 @@
 package com.example.praktikum6.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.praktikum6.ui.view.viewmodel.MahasiswaViewModel
+import com.example.praktikum6.ui.view.viewmodel.RencanaStudyViewModel
+
 enum class Halaman {
     Splash,
     Mahasiswa,
     Matakuliah,
     Tampil
+}
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+)
+{
+
 }
