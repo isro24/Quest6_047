cdcda6aeb420580526065cf4bf55654d690b097e
isro24 <alamat_email_anda@example.com>
2024-11-22 09:26:26 +0700

    Membuat MahasiswaViewModel

--- app/src/main/java/com/example/praktikum6/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/praktikum6/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,4 +1,39 @@
 package com.example.praktikum6.ui.view.viewmodel
 
-class MahasiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.praktikum6.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel : ViewModel() {
+    private val _mhsState = MutableStateFlow(Mahasiswa())
+    val mhsStateUi: StateFlow<Mahasiswa> = _mhsState.asStateFlow()
+
+    fun setNim(inputNim: String){
+        _mhsState.update {
+                stateNim -> stateNim.copy(nim = inputNim)
+        }
+    }
+
+    fun setNama(inputNama: String){
+        _mhsState.update {
+                stateNama -> stateNama.copy(nama = inputNama)
+        }
+    }
+
+    fun setEmail(inputEmail: String){
+        _mhsState.update {
+                stateEmail -> stateEmail.copy(email = inputEmail)
+        }
+    }
+
+    fun saveDataMhs(ls: MutableList<String>){
+        _mhsState.update { status -> status.copy(
+            nim = ls[0],
+            nama = ls[1],
+            email = ls[2]
+        )}
+    }
 }
